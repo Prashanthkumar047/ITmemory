0a09bba9d201d46bec3a5c6a19af650f79eb5ac1
Prashanthkumar047 <48446203+Prashanthkumar047@users.noreply.github.com>
2020-09-20 13:48:09 +0530

    Add files via upload

---
+++ rr.c
@@ -0,0 +1,60 @@
+#include<stdio.h>///////////////////[[[[[[[[-----
+void main(){
+int sum=cout=0;
+int p1=4,p2=5,p3=6,p4=7,cp=3;
+
+    p1=p1-cp;
+    sum=sum+cp;
+    p2=p2-cp;
+    sum=sum+cp;
+    p3=p3-cp;
+    sum=sum+cp;
+    p4=p4-cp;
+    sum=sum+cp;
+    while(1){
+                if(){
+
+                }
+        if(p1>=cp)
+            sum=sum+cp;
+        //else
+          //  sum=sum+p1;
+        p1=p1-cp;
+        if(p1>0)
+            sum=sum+p1;
+        p1=p1-cp;
+        else if(p1<=0)
+            printf("%d",sum);
+
+
+    //
+
+        else printf("%d",sum);
+
+           if(p2>=cp)
+                sum=sum+cp;
+           else
+            sum=sum+p2;
+
+           p2=p2-cp;
+           if(p2<=0)
+            printf("%d",sum);
+
+           if(p3>=cp)
+            sum=sum+cp;
+           else
+            sum=sum+p3;
+
+           p3=p3-cp;
+           if(p3<=0){
+            printf("%d",sum);
+
+           }
+
+    }
+
+
+
+
+
+}
